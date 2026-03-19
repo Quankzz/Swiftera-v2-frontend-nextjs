3777330146d163a737001fc714ef4f465aa66cdc
Quankzz <22h1120134@ut.edu.vn>
2026-03-19 21:55:45 +0700

    feat: Hide wave dot and add Tabs components

--- src/components/common/Waves.tsx
+++ src/components/common/Waves.tsx
@@ -386,7 +386,7 @@ const Waves: React.FC<WavesProps> = ({
       className={`absolute top-0 left-0 w-full h-full overflow-hidden ${className}`}
     >
       <div
-        className="absolute top-0 left-0 bg-[#160000] rounded-full w-[0.5rem] h-[0.5rem]"
+        className="absolute top-0 left-0 rounded-full w-[0.5rem] h-[0.5rem] opacity-0 pointer-events-none"
         style={{
           transform: 'translate3d(calc(var(--x) - 50%), calc(var(--y) - 50%), 0)',
           willChange: 'transform'
